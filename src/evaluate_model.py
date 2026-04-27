132c35d7590eefbfb16222ae3773b58d8333b16e
Ramya Dabbala <ramyasherly06@gmail.com>
2026-04-27 09:48:21 -0500

    Update evaluate_model.py

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,6 +1,8 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 def evaluate(y_true, y_pred):
-   print("Accuracy:", accuracy_score(y_true, y_pred))
-   print("Precision:", precision_score(y_true, y_pred))
-   print("Recall:", recall_score(y_true, y_pred))
-   print("F1 Score:", f1_score(y_true, y_pred))
+   print("Accuracy:", round(accuracy_score(y_true, y_pred), 2))
+   print("Precision:", round(precision_score(y_true, y_pred, zero_division=0), 2))
+   print("Recall:", round(recall_score(y_true, y_pred, zero_division=0), 2))
+   print("F1 Score:", round(f1_score(y_true, y_pred, zero_division=0), 2))
+   print("Confusion Matrix:")
+   print(confusion_matrix(y_true, y_pred))
